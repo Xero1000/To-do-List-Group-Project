1c6bf532b19ffb274ac2f0ecd13da556c719d1e3
Xero1000 <alex.heil@comcast.net>
2023-03-09 20:47:48 -0500

    Got delete and setComplete functions working

--- app.js
+++ app.js
@@ -1,58 +1,87 @@
-
 let textBox = document.querySelector('input');
 let ul = document.querySelector('ul');
 let add_button = document.querySelector('.add_btn');
 
 let list = {
-    items: [],
+    items: [], // an array of objects representing each list item containing the item text and a boolean value for if it's complete
     add: function(text) {
+
+        // Create a new list element and give it the text from the input box
+        let newLI = document.createElement('li');
+        newLI.textContent = text;
+
+        // Create a new child element for the delete (x) button
+        let newLiChild = document.createElement('span');
+        newLiChild.textContent = ' x ';
+        newLiChild.classList.add("delete_btn");
+
+        // delete button is appended to new list item
+        // and new list item is appended to the unordered list
+        newLI.appendChild(newLiChild);
+        ul.appendChild(newLI);
+
+        // object with list item's text and completion status is pushed to 
+        // items array
         this.items.push(
             {
-                content: text, 
+                content: newLI.textContent, 
                 complete: false
             }
         );        
 
-        console.log(this.items);
-        let newLI = document.createElement('li');
-        newLI.textContent = text;
-        let newLiChild = document.createElement('span');
-        newLiChild.textContent = ' x ';
-        newLI.appendChild(newLiChild);
-        ul.appendChild(newLI);
         textBox.value = '';
-    },
-    delete: function(text) {
-        let itemIndex = this.items.indexOf(this.items.find(function(item){
-            return item.content === text;
-        }));
-        this.items.splice(itemIndex, 1);
         console.log(this.items);
-        // ul.children[itemIndex].style.textDecoration = "line-through";
-        // console.log(itemIndex);
-        // ul.children[itemIndex].remove();
     },
-    setComplete: function(text){
+    delete: function(element) {
+
+        // find the index of the object representing the element within the items array
+        // and delete the item from both the items array and the unordered list  
+        let itemIndex = this.items.indexOf(this.items.find(function(item){
+            return item.content === element.textContent;
+        }));
+        
+        this.items.splice(itemIndex, 1);
+        ul.children[itemIndex].remove();
+        console.log(this.items);
+    },
+    setComplete: function(element){
+
+        // Get the element's corresponding object within the items array
+        // and toggle its completion status 
         let item = this.items.find(function(item){
-            return item.content === text;
+            return item.content === element.textContent;
         });
         item.complete = !item.complete;
-        console.log(item);
+        
+        // completed items turn green but the x
+        // button remains the same
+        // if not completed, the item turns black again
+        if (item.complete) {
+            element.style.color = 'green';
+            element.childNodes[1].style.color = 'black';
+        }
+        else {
+            element.style.color = 'black';
+        }
+        console.log(this.items);
     }
 }
 
-// list.add("Clean the House");
-// list.add("Walk the dog");
-// list.add("Take out the trash");
-
-// list.delete("Walk the dog");
-// list.setComplete("Clean the House");
-// console.log(list.items);
-
+// event listener for add button 
 add_button.addEventListener('click', function(event){
     list.add(textBox.value);
 })
 
-// ul.addEventListener('click', function(event){
-//     list.delete(event.target.textContent);
-// })
+// event listener for clicking on an item itself
+ul.addEventListener('click', function(event){
+    
+    // if the delete button was clicked, call the list's delete function
+    if (event.target.classList.contains("delete_btn")){
+        list.delete(event.target.parentElement);
+    }
+
+    // if the list item text was clicked, call the list's setComplete function
+    else if (event.target.tagName === 'LI'){
+        list.setComplete(event.target);
+    }
+})
